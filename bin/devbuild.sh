d76d397621cce23fe1ecf91286b5e8ee19e255d1
Anshu Prateek <anshu.prateek@ril.com>
2015-08-12 09:38:09 +0530

    Finishing dev-build + some minor changes

--- bin/devbuild.sh
+++ bin/devbuild.sh
@@ -1,21 +1,61 @@
 set -xe
 
-BUILD_TAG=anshu-test-tenant-1
-#BUILD_TAG=external-at-2
-#ghprbPullId=725
+BUILD_TAG=anshu-test-tenant-2
+##
+# For creating httpproxy, uncomment the below BUILD_TAG.
+# Your httpproxy BUILD_TAG should be different from your regular build tag, else cleanup
+# will delete your httpproxy as well
+##
+#BUILD_TAG=external-at
+
+##
+# layout=external only for creating httpproxy. Comment out for regular builds
+##
+# export layout=external
+
+##
+# Use this for setting your puppet-rjil PR id
+# Comment out both if you just want to use master branch.
+##
+#ghprbPullId=774
 #export pull_request_id=$ghprbPullId
+
+## Update this to the key-pair name you uploaded to your tenant
 export KEY_NAME=combo
+## Path to your openrc file
 export env_file="/home/anshup/git/anshprat/myfiles/private_keys/anshu_rc"
 export ssh_user=ubuntu
-export dns_server=192.168.20.2
-#export dns_server=10.0.0.2
+## You need to change the dns_server only if the private ip of your httpproxy is different.
+## This should be the private ip of your httpproxy server
+export dns_server=10.0.0.2
 export env_http_proxy=http://${dns_server}:3128/
 export env_https_proxy=http://${dns_server}:3128/
-export cloud_provider=anshu-gate
 
+# You should have an environment/<cloud_provider_name_specified_below>.map.yaml
+# Use environment/jio.map.yaml as a template for your copy and update the network id there
+export cloud_provider=anshu_gate
 
+##
+# Below are only required if you are making changed to python-jiocloud and need to source
+# them from your repo instead of from jiocloud master
+##
+#export python_jiocloud_source_repo=git+https://github.com/anshprat/python-jiocloud
+#export python_jiocloud_source_branch=hkumar_pr_557_giant_repo
+
+##
+# This is required only when you are using your own pkgs build via pkg_test_build or consul-gate
+##
 #export override_repo="http://jiocloud.rustedhalo.com:8080/job/pkg_test_build/69/artifact/new_repo.tgz"
-#export layout=external
+
+##
+# This is required to be changed only when you are making changes to
+# puppet rjil and want to test those changes from your repo without opening PR
+# This and PR id should not be used together.
+# ensure that the correct pull request number and repo are set
+##
+export puppet_modules_source_repo=https://github.com/anshprat/puppet-rjil
+export puppet_modules_source_branch=test-tenant-upgrade
+##
 
 ######################## END OF MODIFICATIONS ################################
 
@@ -26,13 +66,6 @@ export BUILD_NUMBER=$BUILD_TAG
 # use the acceptance test environment (I may have to patch this a little)
 export module_git_cache=http://jiocloud.rustedhalo.com:8080/job/puppet-jiocloud-cache/lastSuccessfulBuild/artifact/cache.tar.gz
 export env=at
-export cloud_provider=jio
-##
-# We should use combo keys, in which case bodepd/sorenh/hkumar/jenkins will have ssh access as ubuntu user,
-# so commenting KEYNAME variable, for now which makes it to use combo (Dated: 2015-03-19) - Harish
-##
 
-# ensure that the correct pull request number and repo are set
-export puppet_modules_source_repo=https://github.com/jiocloud/puppet-rjil
 ./build_scripts/deploy.sh
 ./build_scripts/test.sh
