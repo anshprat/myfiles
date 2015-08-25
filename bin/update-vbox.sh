2c9f3cd8965ff924f0376ce1e52915c485d1a797
Anshu Prateek <anshu.prateek@ril.com>
2015-08-25 20:53:37 +0530

    Updating to fedora 22

--- bin/update-vbox.sh
+++ bin/update-vbox.sh
@@ -6,8 +6,7 @@ latest_version=`curl -s http://download.virtualbox.org/virtualbox/LATEST.TXT`
 
 if [ ${installed_version} != ${latest_version} ]
 then
-rpm=`curl -sL http://download.virtualbox.org/virtualbox/${latest_version}|grep 'fedora18-1.x86_64.rpm'|awk -F '"' '{print $2}'`
-sudo yum install -y http://download.virtualbox.org/virtualbox/${latest_version}/${rpm}
+rpm=`curl -sL http://download.virtualbox.org/virtualbox/${latest_version}|grep 'fedora22-1.x86_64.rpm'|awk -F '"' '{print $2}'`
+sudo dnf install -y http://download.virtualbox.org/virtualbox/${latest_version}/${rpm}
 fi
-
-
+sudo /etc/init.d/vboxdrv setup
