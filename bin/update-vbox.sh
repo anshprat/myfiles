5e273272758c4b4813d8b3aad94119a842197ff0
Anshu Prateek <anshprat@gmail.com>
2016-02-14 18:50:20 +0530

    Fix empty string comparison
    When vbox is not installed, present version check result is empty
    this needs to be sanitised so that the script can install even if its not an update.
    	modified:   update-vbox.sh

--- bin/update-vbox.sh
+++ bin/update-vbox.sh
@@ -4,9 +4,9 @@ installed_version=`VBoxManage --version|cut -f 1 -d 'r'`
 
 latest_version=`curl -s http://download.virtualbox.org/virtualbox/LATEST.TXT`
 
-if [ ${installed_version} != ${latest_version} ]
+if [ "${installed_version}" != ${latest_version} ]
 then
 rpm=`curl -sL http://download.virtualbox.org/virtualbox/${latest_version}|grep 'fedora22-1.x86_64.rpm'|awk -F '"' '{print $2}'`
 sudo dnf install -y http://download.virtualbox.org/virtualbox/${latest_version}/${rpm}
 fi
-sudo /etc/init.d/vboxdrv setup
+sudo /sbin/rcvboxdrv setup
