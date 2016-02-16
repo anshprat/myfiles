5d06875657dd23f74e801d96b7dabcc6b1f77757
Anshu Prateek <anshprat@gmail.com>
2016-02-16 15:25:40 +0530

    Add requirements for vbox
    This is helpful when its a fresh install
    
    	modified:   update-vbox.sh

--- bin/update-vbox.sh
+++ bin/update-vbox.sh
@@ -6,6 +6,7 @@ latest_version=`curl -s http://download.virtualbox.org/virtualbox/LATEST.TXT`
 
 if [ "${installed_version}" != ${latest_version} ]
 then
+sudo dnf install -y binutils qt gcc make patch libgomp glibc-headers glibc-devel kernel-headers kernel-devel dkms
 rpm=`curl -sL http://download.virtualbox.org/virtualbox/${latest_version}|grep 'fedora22-1.x86_64.rpm'|awk -F '"' '{print $2}'`
 sudo dnf install -y http://download.virtualbox.org/virtualbox/${latest_version}/${rpm}
 fi
