a93908dfb3c4a7e25bdd467bd41a22aee7b998d5
Anshu Prateek <anshu.prateek@freecharge.com>
2017-02-04 12:47:20 +0530

    Add profile changes and docker zabbix
    	modified:   bash_profile
    	new file:   scripts/zabbix_docker.sh

--- bash_profile
+++ bash_profile
@@ -15,7 +15,7 @@ export HISTSIZE=10000
 export HISTFILESIZE=100000
 alias all='screen -d -r all||screen -S all'
 export PATH=$PATH:/usr/local/bin:./
-export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+#export PROMPT_COMMAND='history -a; history -c; history -r; $PROMPT_COMMAND'
 #alias ssh='ssh -A'
 alias p8='ping 8.8.8.8'
 export KEYS=$HOME/code/anshprat/myfiles/private_keys
@@ -28,6 +28,7 @@ alias sc="cd $GIT/sc"
 alias scd="cd $GIT/sc/docker/web.2/scweb"
 alias scr="cd $GIT/sc/screact"
 #alias /usr/lib64/firefox/plugin-container='optirun /usr/lib64/firefox/plugin-container.orig'
-export PS1=" ${debian_chroot:+($debian_chroot)}:\w\>"
+export PS1="\h ${debian_chroot:+($debian_chroot)}:\w\>"
 complete -W "$(echo `cat ~/.ssh/known_hosts | cut -f 1 -d ' ' | sed -e s/,.*//g | uniq | grep -v "\["`;)" ssh
 #~/bin/ff-sync
+export HISTCONTROL=ignoredups:erasedups 
---
+++ scripts/zabbix_docker.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#set -x
+zabbix_mysql_data=$HOME/docker_root/var/lib/mysql
+mkdir -p $zabbix_mysql_data
+
+docker run -p 8080:8080 --name zabbix-mysql -v $zabbix_mysql_data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=mysql -d mysql:8.0
+
+
+#Passive check agent
+docker run -p 8081:8080 --name zabbix-agent-passive -e ZBX_HOSTNAME="zabbix-agent-passive"  -d zabbix/zabbix-agent
+
+#Main server with link to above agent for passive check
+docker run -p 8085:8080 --link zabbix-mysql:zabbix-mysql-server  --link zabbix-agent-passive:zabbix-agent --name zabbix-server -e DB_SERVER_HOST="zabbix-mysql-server" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD="zabbixpassword" -d -p 10051:10051 -p 162:162/udp zabbix/zabbix-server-mysql:latest
+
+
+#Active check agent
+
+docker run -p 8083:8080 --name zabbix-agent-active --link zabbix-server:zabbix-server -d zabbix/zabbix-agent:latest
+
+
+# Zabbix Web UI
+docker run -p 8084:8080 --name zabbix-web-nginx-mysql  --link zabbix-server:zabbix-server --link zabbix-mysql:zabbix-mysql-server -e DB_SERVER_HOST="zabbix-mysql-server" -e MYSQL_USER="zabbix" -e MYSQL_PASSWORD="zabbixpassword" -e ZBX_SERVER_HOST="zabbix-server" -e TZ="UTC" -d -p 8082:80 zabbix/zabbix-web-nginx-mysql
+
+
+
+
+echo "waiting for mysql to comeup and then add creds"
+
+sleep_wait=60
+for i in `seq 1 ${sleep_wait}`; do printf '\b%.0s' {1..60};echo -ne  "waiting for $((60-$i)) sec"; sleep 1; 
+
+  docker exec -it zabbix-mysql mysql -u root -pmysql -e 'use mysql;grant all on *.* to zabbix identified by "zabbixpassword"; flush privileges;' 
+  if [ $? -eq 0 ]
+  then
+    sleep 5
+    open http://127.0.0.1:8082/
+    exit 0
+  fi
+
+done
+
+
