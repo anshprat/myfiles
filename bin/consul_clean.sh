58844ac39579429e43551f186701ca2fb1e52b81
Anshu Prateek <anshu.prateek@grabtaxi.com>
2019-10-06 09:47:35 +0530

    add token

--- bin/consul_clean.sh
+++ bin/consul_clean.sh
@@ -1,4 +1,4 @@
-consul_token=consul_token
+consul_token=consul_token_acl
 file_name=consul.txt
 docker exec -it -e CONSUL_TOKEN=${consul_token} consul consul members -detailed list >${file_name} 
 for id in `grep -o id=.................................... ${file_name} |sort|uniq -c|sort -n|grep -ve "^      1"|awk '{print $2}'`; do echo $id ; grep $id ${file_name} ; done
